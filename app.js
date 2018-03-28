d8c37653071a4020bdb4f0bf280e123b5731be7c
wickning1 <git@mythgaming.net>
2018-03-28 13:11:51 -0500

    use mongo username and password if set in environment variables

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ var db_port = process.env.DB_PORT || '27017';
 var db_user = process.env.DB_USER || '';
 var db_pw = process.env.DB_PASSWORD || '';
 var db_name = process.env.DB_DATABASE || 'faculty_profiles';
-mongoose.connect('mongodb://'+db_host+':'+db_port+'/'+db_name);
+var db_userpassword_prefix = '';
+if (db_user.length > 0 && db_pw.length > 0) db_userpassword_prefix = db_user+':'+db_pw+'@';
+mongoose.connect('mongodb://'+db_userpassword_prefix+db_host+':'+db_port+'/'+db_name);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
