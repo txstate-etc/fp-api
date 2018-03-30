b52570b0fcb6fa7ec8b6b94d7e464c05a64369dc
wickning1 <git@mythgaming.net>
2018-03-30 14:58:08 -0500

    add the ability to specify a mongo authentication database as an environment variable

--- app.js
+++ app.js
@@ -20,12 +20,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 var mongoose = require('mongoose');
 var db_host = process.env.DB_HOST || 'localhost';
 var db_port = process.env.DB_PORT || '27017';
+var db_authdb = process.env.DB_AUTHDATABASE || '';
 var db_user = process.env.DB_USER || '';
 var db_pw = process.env.DB_PASSWORD || '';
 var db_name = process.env.DB_DATABASE || 'faculty_profiles';
 var db_userpassword_prefix = '';
 if (db_user.length > 0 && db_pw.length > 0) db_userpassword_prefix = db_user+':'+db_pw+'@';
-mongoose.connect('mongodb://'+db_userpassword_prefix+db_host+':'+db_port+'/'+db_name);
+var db_authdb_suffix = '';
+if (db_authdb.length > 0) db_authdb_suffix = '?authSource='+db_authdb;
+mongoose.connect('mongodb://'+db_userpassword_prefix+db_host+':'+db_port+'/'+db_name+db_authdb_suffix);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
