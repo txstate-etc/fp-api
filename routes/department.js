ca1d6ab64a592081acf4263f5d299f239bbe730d
Anne Faber <anne.faber@txstate.edu>
2018-04-09 12:06:19 -0500

    Add endpoint to get colleges and departments from people collection

--- routes/department.js
+++ routes/department.js
@@ -3,7 +3,40 @@ var router = express.Router();
 
 router.route('/')
   .get(function(req, res, next) {
-    res.json({message: "Returning sorted list of colleges and departments for search dropdown"})
+    var Person = require('../models/person');
+    return Person.aggregate(
+      [
+        {"$unwind" : "$positions"},
+        {"$match" : {"positions.organization.is_academic": true}},
+        {"$group" : {"_id" : {"college": "$positions.organization.college", "department": "$positions.organization.department"}}},
+        {"$sort" : {"_id.college" : 1, "_id.department" : 1}}
+      ]
+    )
+    .then(function(results) {
+      if (results) {
+        var colleges = {};
+        var organizations = [];
+        results.forEach(function(result) {
+          var college = result["_id"].college;
+          if (!colleges[college]) {
+            colleges[college] = [];
+            organizations.push({"college": college, "departments" : []})
+          }
+          colleges[college].push(result["_id"].department)
+        })
+        organizations.forEach(function(org) {
+          var college = org.college;
+          org.departments = colleges[college];
+        })
+        res.json(organizations)
+      }
+      else {
+        res.json([])
+      }
+    })
+    .catch(function(err) {
+      next(err)
+    })
   })
 
 module.exports = router;
