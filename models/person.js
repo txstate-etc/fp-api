37b6c05ace9ba4ad3d8a2dab4258d1663f75e99e
wickning1 <git@mythgaming.net>
2018-05-09 16:16:26 -0500

    index on lname_words_version since we use it every 9 seconds

--- models/person.js
+++ models/person.js
@@ -46,6 +46,7 @@ var PersonSchema = new Schema({
 PersonSchema.index({user_id: 1});
 PersonSchema.index({UPLOAD_PHOTO: 1});
 PersonSchema.index({cached_face_detection_version: 1});
+PersonSchema.index({lname_words_version: 1});
 PersonSchema.index({lname_words: 1}, {collation: {locale: 'en_US', strength: 2}});
 
 PersonSchema.virtual('display_name').get(function () {
